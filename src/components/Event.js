0d7b5a2134942ffac1257478d81d45368e578125
seth <seth.j.lauer@gmail.com>
2020-04-01 17:28:51 -0700

    Simple list with event titles and a map if coordinates exist

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Map from "./Map";
 
 const Event = props => {
   const { event } = props;
-  return <p>{event && event.title}</p>;
+  if (!event.location || !event.location.location) {
+    return (
+      <p>
+        <div>{event && event.title}</div>
+        <div>{"virtual"}</div>
+      </p>
+    );
+  } else {
+    return (
+      <div style={{ width: "100%", height: "400px" }}>
+        <div>{event && event.title}</div>
+        <Map
+          lat={event.location.location.latitude}
+          lng={event.location.location.longitude}
+        />
+      </div>
+    );
+  }
 };
 
 export default Event;
---
+++ src/components/Map.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+const API_KEY = "AIzaSyCWIN8oiZwKrQ9NPe1XQmZPeyss5JQAf-0";
+
+const EventMarker = ({ text }) => (
+  <div
+    style={{
+      color: "white",
+      background: "green",
+      padding: "15px 10px",
+      display: "inline-flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "100%",
+      transform: "translate(-50%, -50%)"
+    }}
+  >
+    {text}
+  </div>
+);
+
+class Map extends Component {
+  static defaultProps = {
+    lat: 0,
+    lng: 0,
+    zoom: 11
+  };
+
+  render() {
+    const { lat, lng, zoom } = this.props;
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: API_KEY
+        }}
+        defaultCenter={{ lat, lng }}
+        defaultZoom={zoom}
+      >
+        <EventMarker lat={lat} lng={lng} text="X" />
+      </GoogleMapReact>
+    );
+  }
+}
+
+export default Map;
